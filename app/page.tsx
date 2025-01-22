2738df00cba8a254115190945be06e05cb116764
Jialin Yang <19481802+yangjialin94@users.noreply.github.com>
2025-01-22 16:26:27 -0500

    Update mobile padding

--- app/page.tsx
+++ app/page.tsx
@@ -98,7 +98,7 @@ const DownloadResume = () => {
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col items-start justify-center flex-1 gap-10 px-4 pb-16 sm:px-8 sm:pb-20 md:px-16 lg:px-20">
+      <main className="flex flex-col items-start justify-center flex-1 gap-10 px-8 py-10 sm:px-8 sm:pb-20 md:px-16 lg:px-20">
         <Hero />
         <Stats />
         <DownloadResume />
