a5054f2403fe658cd61a129ee3d9307c1a343207
Jialin Yang <19481802+yangjialin94@users.noreply.github.com>
2025-01-24 18:55:45 -0500

    finished 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,56 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import Image from 'next/image';
 import React from 'react';
+
+import Footer from '@/components/footer';
 
 export default function NotFound() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <h1>404</h1>
+    <>
+      {/* Main */}
+      <main className="flex flex-1 flex-col items-center justify-center gap-10 px-6 py-10 sm:px-8 sm:pb-20 md:px-16 lg:px-20">
+        <motion.div
+          className="relative h-48 w-48 overflow-hidden rounded-full"
+          animate={{
+            scale: [1, 1.6, 1],
+          }}
+          transition={{
+            repeat: Infinity,
+            duration: 2,
+            ease: 'easeInOut',
+          }}
+        >
+          <Image
+            src="/images/jialin-122324.jpg"
+            alt="Not Found Image"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          />
+        </motion.div>
+
+        <motion.h1
+          className="my-4 text-center text-4xl font-bold text-gray-900 hover:text-red-500 dark:text-gray-100 dark:hover:text-red-500 md:my-12"
+          initial={{ scale: 1, rotate: 0 }}
+          animate={{ scale: 1, rotate: 0 }}
+          whileHover={{
+            scale: 1.5,
+            rotate: 15,
+          }}
+          transition={{
+            type: 'spring',
+            stiffness: 300,
+            damping: 20,
+          }}
+        >
+          Nothing to see here 🙄
+        </motion.h1>
       </main>
-    </div>
+
+      {/* Footer */}
+      <Footer page="Experiences" />
+    </>
   );
 }
