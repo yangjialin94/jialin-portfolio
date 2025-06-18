0655f3712ff003db3af149f3a00a9da53bb3e02b
Jialin Yang <19481802+yangjialin94@users.noreply.github.com>
2025-06-18 12:32:38 -0400

    udpated new resume

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -67,7 +67,7 @@ const SideBar = ({ page }: SideBarProps) => {
           transition: { duration: 0.2, ease: "easeOut" },
         }}
         className={tabClassName}
-        href="/files/Resume_Jialin_060525.pdf"
+        href="/files/Jialin_Resume_061825.pdf"
         download="Jialin_Resume.pdf"
         target="_blank"
         rel="noopener noreferrer"
